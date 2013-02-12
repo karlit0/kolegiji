68e4ab89820b1014b1e05b306a66e69c40d530a3
Karlit0 <antonio.stipanovic@fer.hr>
2013-02-13 00:20:00 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kolegiji::Application.config.secret_token = 'ce915c909e532739d7072a215acd05c2be9615835d61db305fd57a458c9041088cf71b6975fd6dc66d9a8ba4e67b4985b81f851bd2b9f78e60ed22ebb882e7c2'
